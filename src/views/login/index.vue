bb4734b50c65d6934296e3163d8ce44ca7b21f0c
rsl140 <rsl140@qq.com>
2019-08-08 17:54:34 +0800

    chore: add babel-polyfill

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -64,7 +64,6 @@ export default {
       try {
         const data = await this.getUserInfo(this.login)
         this.resData = data.data.account
-        alert('click')
         this.$router.push({ name: 'Playlist' })
         // 关闭loading
         this.closeLoading()
